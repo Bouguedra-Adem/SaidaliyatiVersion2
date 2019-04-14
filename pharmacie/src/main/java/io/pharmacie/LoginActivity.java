d698eb5649326ce77bc4776b1a42add78eab4818
Mustapha Amine <fm_debbih@esi.dz>
2019-04-14 15:23:06 +0200

    First commit

---
+++ pharmacie/src/main/java/io/pharmacie/LoginActivity.java
@@ -0,0 +1,52 @@
+package io.pharmacie;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final int REQUEST_SIGNUP = 0;
+
+    @BindView(R.id.link_signup) TextView _signupLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        _signupLink.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // Start the Signup activity
+            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+            startActivityForResult(intent, REQUEST_SIGNUP);
+            finish();
+            overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+        }
+    });
+    }
+
+    public String generatePwd(int length)
+    {
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String pass = "";
+        for(int x=0;x<length;x++)
+        {
+            int i = (int)Math.floor(Math.random() * 62);
+            pass += chars.charAt(i);
+        }
+        System.out.println(pass);
+        return pass;
+    }
+}
---
+++ pharmacie/src/main/java/io/pharmacie/SignupActivity.java
@@ -0,0 +1,33 @@
+package io.pharmacie;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @BindView(R.id.link_login) TextView _loginLink;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        _loginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the registration screen and return to the Login activity
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+            }
+        });
+    }
+}
