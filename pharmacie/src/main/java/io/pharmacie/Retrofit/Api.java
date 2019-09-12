ee93f31ed0741f7de623e9838971fe3e318c0889
Bouguedra-Adem <32870464+Bouguedra-Adem@users.noreply.github.com>
2019-09-12 18:28:05 +0200

    update API

--- pharmacie/src/main/java/io/pharmacie/Retrofit/Api.java
+++ pharmacie/src/main/java/io/pharmacie/Retrofit/Api.java
@@ -1,4 +1,40 @@
+
 package io.pharmacie.Retrofit;
+
+import java.util.List;
+import io.pharmacie.models.Commune;
+import io.pharmacie.models.User;
+import io.pharmacie.models.pharmacy;
+import io.reactivex.Observable;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface Api {
+
+    String Base_Url = "https://tranquil-escarpment-61375.herokuapp.com/";
+    //String Base_Url = "http://10.0.2.2:3000/";
+
+    @GET("/login/{useremail}/{pwd}")
+    Call<User> loginUser(@Path(value="useremail") String email,
+                         @Path(value="pwd") String pwd);
+
+    @GET("/communes")
+    Call<List<Commune>> getCommunes();
+
+    @GET("/pharmacies/{commune}")
+    Call<List<pharmacy>> getPharmacies(@Path(value="commune",encoded = true) String commune);
+
+    @GET("/pharmaciegarde/{dateGarde}")
+    Call<List<pharmacy>> getPharmaciesGarde(@Path(value="dateGarde",encoded = true) String dateGarde);
+
+    @POST("/users")
+    Call<User> createAccount(@Body User user);
+}/*package io.pharmacie.Retrofit;
 
 import java.util.List;
 import io.pharmacie.models.Commune;
@@ -37,4 +73,5 @@ public interface Api {
 
 
 
-}
+}*/
+
